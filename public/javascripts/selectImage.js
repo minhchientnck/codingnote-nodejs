4ff78eeddcf1bc15cb1c90fc87c842aee6881e49
unknown <minhchientnck>
2021-09-02 00:23:56 +0700

    update code

--- public/javascripts/selectImage.js
+++ public/javascripts/selectImage.js
@@ -10,3 +10,17 @@ function returnFileUrl(fileUrl) {
   window.opener.CKEDITOR.tools.callFunction(funcNum, fileUrl);
   window.close();
 }
+
+
+$(document).ready(function() {
+ $('.arrows-move').click(function() {
+   var url = $(this).attr('data-value');
+   console.log($('.view-image'))
+   $('.view-image').css('display', 'block');
+   $('.view-image img').attr('src', url);
+ });
+
+ $('.view-image .close').click(function() {
+  $('.view-image').css('display', 'none');
+ })
+});
